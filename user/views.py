b0281679780ec420b3703e5c1924382b96bcdcd6
ivanw-yu <ivnyu@mail.sfsu.edu>
2018-08-31 00:31:06 -0700

    fixed importation and indentation bugs in user/views.py

--- user/views.py
+++ user/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from rest_framework import status
 
 from .serializers import RegistrationSerializer
 
 # Create your views here.
 class RegistrationAPIView(APIView):
-    """ Handles logic for registering user using
+     """ Handles logic for registering user using
         the RegistrationSerializer to create, validate and save
         User objects.
      """
