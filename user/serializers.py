c564daee4ab1e1c4c3bbc064db8cac9a1a52d551
ivanw-yu <ivnyu@mail.sfsu.edu>
2018-09-01 00:39:01 -0700

    Implemented Login serializer.

--- user/serializers.py
+++ user/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 from .models import User
 
 class RegistrationSerializer(serializers.ModelSerializer):
@@ -34,3 +35,41 @@ class RegistrationSerializer(serializers.ModelSerializer):
             of UserManager.
         """
         return User.objects.create_user(**user)
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(min_length=5,
+                                     max_length=50,
+                                     write_only=True)
+
+    token = serializers.CharField(max_length=300)
+
+    def validate(self, data):
+
+        email = data.get('email')
+        password = data.get('password')
+
+        if email is None:
+            raise serializer.ValidationError(
+                'Email is required.'
+            )
+
+        if password is None:
+            raise serializer.ValidationError(
+                'Password is required.'
+            )
+
+        # authenticate returns user if the username and password
+        # checks out, and None otherwise
+        user = authenticate(username=email, password=password)
+
+        if user is None:
+            raise serializer.ValidationError(
+                'Email and password combination invalid.'
+            )
+
+        return {
+            'user': user,
+            'token': user.token
+        }
