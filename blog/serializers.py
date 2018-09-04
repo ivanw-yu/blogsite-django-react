7ffaffbe52488ee1cf2b7f1806a05a251dca5540
ivanw-yu <ivnyu@mail.sfsu.edu>
2018-09-04 02:48:37 -0700

    Included created and edited fields of Blog as read_only fields in BlogSerializer.

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,10 +2,14 @@ from rest_framework import serializers
 from .models import Blog
 
 class BlogSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Blog
         fields = ('title',
                   'content',
                   'user',
-                  'id')
+                  'id',
+                  'created',
+                  'edited')
+
+        read_only_fields = ('created', 'edited')
